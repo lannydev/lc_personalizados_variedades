d3e4069d95906a136c83098f861febc5829f6b62
Elaine <e.cristina00@gmail.com>
2023-07-23 23:35:49 -0300

    Material table para listar os personalizados criado.

--- personalizados-ui/src/app/personalizados/personalizados/personalizados.component.ts
+++ personalizados-ui/src/app/personalizados/personalizados/personalizados.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Personalizado } from '../model/personalizado';
 
 @Component({
   selector: 'app-personalizados',
   templateUrl: './personalizados.component.html',
   styleUrls: ['./personalizados.component.scss']
 })
-export class PersonalizadosComponent {
+export class PersonalizadosComponent implements OnInit{
+
+  personalizados: Personalizado[] = [
+    {_id: '1', nome: 'Tags Minnie', categoria: 'Personalizados Impressos'},
+    {_id: '2', nome: 'Toalhinhas bordadas com nome e desenho', categoria: 'Bordado Manual'}
+  ];
+  displayedColumns = ['nome', 'categoria'];
+  
+  constructor(){
+
+    //this.personalizados = [];
+
+  }
+
+  ngOnInit(): void {
+    
+  }
+
 
 }
